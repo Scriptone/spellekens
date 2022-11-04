4f8d3e80f50d4eeabe09ad8fabb01efde6a5c090
Scriptone <arne.haers@student.odisee.be>
2022-11-04 12:39:23 +0100

    1

--- about/php/formchecking_thanks.php
+++ about/php/formchecking_thanks.php
@@ -1,4 +1,26 @@
 <?php
+// Constanten (connectie-instellingen databank)
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'contact2');
+
+
+date_default_timezone_set('Europe/Brussels');
+
+// Verbinding maken met de databank
+try {
+    $db = new PDO('mysql:host=' . DB_HOST .';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Verbindingsfout: ' .  $e->getMessage();
+    exit;
+}
+
+// Opvragen van alle taken uit de tabel tasks
+$stmt = $db->prepare('SELECT * FROM messages ORDER BY added_on DESC');
+$stmt->execute();
+$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $name = isset($_GET['name']) ? $_GET['name'] : false;
 $mail = isset($_GET['mail']) ? $_GET['mail'] : false;
