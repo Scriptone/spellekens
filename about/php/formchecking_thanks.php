a17a1d63cbe38e6a2c546f4b6f130c5800c55a67
Scriptone <arne.haers@student.odisee.be>
2022-11-06 17:09:46 +0100

    1

--- about/php/formchecking_thanks.php
+++ about/php/formchecking_thanks.php
@@ -24,7 +24,7 @@ $name = isset($_GET["name"]) ? $_GET["name"] : false;
 <body>
     <header>
         <a href="../../" class="logo">
-            <h1><img src="../images/logo.png" alt="Logo spellekens" width="100"> Spellekens.be</h1>
+            <h1><img src="../../images/logo.png" alt="Logo spellekens" width="100"> Spellekens.be</h1>
         </a>
         <nav>
             <ul class="nav">
