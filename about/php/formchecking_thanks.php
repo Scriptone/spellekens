51d35ba12ba4fda873dc27f7da8d0739c6756d7e
Scriptone <arne.haers@student.odisee.be>
2022-11-06 14:14:45 +0100

    1

--- about/php/formchecking_thanks.php
+++ about/php/formchecking_thanks.php
@@ -8,7 +8,7 @@ $name = isset($_GET["name"]) ? $_GET["name"] : false;
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width = device-width">
-    <link rel="icon" type="image/x-icon" href="../logo.png">
+    <link rel="icon" type="image/x-icon" href="../../logo.png">
 
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
         integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
@@ -16,7 +16,7 @@ $name = isset($_GET["name"]) ? $_GET["name"] : false;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../../css/styles.css">
     <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
         integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
     <title>Contact form</title>
