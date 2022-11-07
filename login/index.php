99f37a364489bfd6393b1a9ce9dafb7f5dc8e9d3
Scriptone <arne.haers@student.odisee.be>
2022-11-07 13:14:19 +0100

    Test

--- login/index.php
+++ login/index.php
@@ -59,9 +59,7 @@ if (isset($_POST['submit'])) {
 			<div class="input-groep">
 					<button name="submit" class="btn">
 					Login
-					<!--svg width="16" height="16" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
-  <path d="M8 0c4.4 0 8 3.6 8 8s-3.6 8-8 8-8-3.6-8-8 3.6-8 8-8zm0 12c-1.6 0-3.2.4-4.6 1 1.2 1.1 2.8 1.8 4.6 1.8 1.8 0 3.4-.7 4.6-1.8-1.5-.7-3-1-4.6-1zM8 1.3c-3.7 0-6.7 3-6.7 6.7 0 1.5.5 2.8 1.3 3.9 1.7-.8 3.6-1.2 5.5-1.2s3.8.4 5.5 1.2c.8-1.1 1.2-2.4 1.3-3.8v-.4c-.2-3.5-3.2-6.4-6.9-6.4zm0 1.4c1.8 0 3.3 1.5 3.3 3.3S9.8 9.3 8 9.3 4.7 7.8 4.7 6 6.2 2.7 8 2.7zm0 1.2c-1.1 0-2.1 1-2.1 2.1s1 2 2.1 2 2-.9 2.1-1.9v-.2c-.1-1.1-1-2-2.1-2z"></path>
-</svg-->				</div>
+			</div>
 			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
 			<div class="input-groep">
 
